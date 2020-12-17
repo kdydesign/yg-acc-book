dbda39b5335b67a8a5160aa72414d57e030b13e8
kdydesign <kdydesign@gmail.com>
2020-12-17 16:45:29 +0900

    initial commit

---
+++ src/layouts/HeaderTools.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-header elevated>
+    <q-toolbar>
+      <q-toolbar-title>
+        YG Account Book
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderTools'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-page class="q-layout-padding card-examples row items-start q-gutter-md">
+    <q-card v-for="i in 2"
+            :key="i">
+      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+        <div class="absolute-bottom">
+          <div class="text-h6">
+            오창 명종이네 집
+          </div>
+          <div class="text-subtitle2">
+            2020-12-12
+          </div>
+        </div>
+      </q-img>
+
+      <q-card-actions>
+        <q-btn flat>
+          Action 1
+        </q-btn>
+        <q-btn flat>
+          Action 2
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+  },
+  computed: {
+    tableEl () {
+      return this.$refs.table ? this.$refs.table.$el : null
+    }
+  }
+}
+</script>
